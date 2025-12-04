cf623c8d92015f2ccaa28188f9909b8509c2a29e
xPuma7x <julian.kenk05@gmail.com>
2025-12-04 21:32:22 +0100

    edited paths and filenames

--- src/data_cleaning/clean_dataset.py
+++ src/data_cleaning/clean_dataset.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("grosshandelpreise_2022_2025_15min.csv", sep=";")
+df = pd.read_csv("../../data/grosshandelpreise_2022_2025_15min.csv", sep=";")
 df = df.dropna(how="all")
 
 columns_to_keep = ["Datum von","Datum bis","Deutschland/Luxemburg [€/MWh] Originalauflösungen"]
 df = df[columns_to_keep]
 
-df.to_csv('filtered_output.csv', index=False)
+df.to_csv('../../data/fact_liferantenpreis.csv', index=False)
 
-print("Filtered file saved as 'filtered_output.csv'")
+print("Filtered file saved as 'fact_liferantenpreis.csv'")
